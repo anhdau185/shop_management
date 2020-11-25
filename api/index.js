7ecba76f931a541aa28ed9c16956f969aa4be973
Aaron Dau <aaron.dau@globee.hk>
2020-11-25 10:44:09 +0700

    Prepare API module for making network requests

---
+++ api/index.js
@@ -0,0 +1,73 @@
+import { schemes, httpMethods, headers } from './staticEntries';
+
+const scheme = schemes.HTTPS;
+const host = '134d19ca4777.ap.ngrok.io';
+const basePath = '/api/v1';
+const paths = {
+    get: {
+        menu: '/menu',
+        test: '/posts/1'
+    },
+    post: {
+        authPhone: '/customers/phone_login',
+        authOtp: '/customers/verify_code',
+        createOrder: '/orders'
+    }
+};
+
+function getApiPath(path, param = null) {
+    let apiPath = scheme + host + basePath + path;
+    if (param) {
+        apiPath += param;
+    }
+    return apiPath;
+}
+
+function getConfigurations(method, data = null) {
+    let configurations = {
+        method,
+        headers
+    };
+    if (data) {
+        configurations.body = JSON.stringify(data);
+    }
+    return configurations;
+}
+
+/* export const fetchStoreMenu = async () => {
+    const apiPath = getApiPath(paths.get.menu);
+    const configurations = getConfigurations(httpMethods.GET);
+    const response = await fetch(apiPath, configurations);
+    const data = await response.json();
+    return data;
+};
+
+export const authenticatePhone = async phoneNumber => {
+    const apiPath = getApiPath(paths.post.authPhone);
+    const configurations = getConfigurations(httpMethods.POST, { phoneNumber });
+    const response = await fetch(apiPath, configurations);
+    const data = await response.json();
+    return data;
+};
+
+export const authenticateOtp = async (phoneNumber, otp) => {
+    const apiPath = getApiPath(paths.post.authOtp);
+    const configurations = getConfigurations(
+        httpMethods.POST,
+        {
+            phoneNumber,
+            codeVerify: otp
+        }
+    );
+    const response = await fetch(apiPath, configurations);
+    const data = await response.json();
+    return data;
+};
+
+export const createOrder = async order => {
+    const apiPath = getApiPath(paths.post.createOrder);
+    const configurations = getConfigurations(httpMethods.POST, order);
+    const response = await fetch(apiPath, configurations);
+    const data = await response.json();
+    return data;
+}; */
---
+++ api/staticEntries.js
@@ -0,0 +1,18 @@
+export const schemes = {
+    HTTP: 'http://',
+    HTTPS: 'https://'
+};
+
+export const httpMethods = {
+    GET: 'GET',
+    POST: 'POST',
+    PUT: 'PUT',
+    PATCH: 'PATCH',
+    DELETE: 'DELETE'
+}
+
+export const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnQiOiJVSVQgU0hPUCJ9.MQiLgrZ9EjFMjTPs2Br8WKFFzYRjL7bPC6MJWwFF9QA'
+};
