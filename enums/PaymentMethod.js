91daa294f41681931bd1e464af728667a1071850
Aaron Dau <aaron.dau@globee.hk>
2020-12-02 10:58:18 +0700

    Add new enums files

---
+++ enums/PaymentMethod.js
@@ -0,0 +1,16 @@
+const PaymentMethod = {
+    MOMO: {
+        value: 'MOMO',
+        shortTitle: 'Ví MoMo',
+        longTitle: 'Thanh toán bằng ví điện tử MoMo',
+        indicatorColor: '#a51f68'
+    },
+    COD: {
+        value: 'COD',
+        shortTitle: 'Tiền mặt',
+        longTitle: 'Thanh toán bằng tiền mặt khi nhận hàng',
+        indicatorColor: '#4db856'
+    }
+};
+
+export default PaymentMethod;
